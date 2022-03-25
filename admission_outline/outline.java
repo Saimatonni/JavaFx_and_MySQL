ed1fec9571717a48f6e46a1e9b85505c4951edfe
saima <74361691+Saimatonni@users.noreply.github.com>
2022-03-25 18:53:40 +0600

    Add files via upload

---
+++ admission_outline/outline.java
@@ -0,0 +1,14 @@
+package com.example.admission_dash;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class outline {
+    /*@FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }*/
+}
